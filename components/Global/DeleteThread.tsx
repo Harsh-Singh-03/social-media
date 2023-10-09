697c7dc0a5c4073d7e8ecd9a89c19574c9be7ad0
Harsh Singh <harshnov802@gmail.com>
2023-10-09 17:44:01 +0530

    fixing loader in delete thread fuction

--- components/Global/DeleteThread.tsx
+++ components/Global/DeleteThread.tsx
@@ -4,15 +4,19 @@ import { usePathname } from "next/navigation"
 import { deleteThread } from "@/server/actions/thread.actions"
 import { useToast } from "../ui/use-toast"
 import Image from "next/image"
+import { useLoader } from "../ui/LoaderContext"
 
 interface props {
   id: string
 }
 const DeleteThread = ({ id }: props) => {
+  const {hideLoader, showLoader}: any = useLoader()
   const { toast } = useToast()
   const path = usePathname()
   const deletePost = async () => {
+    showLoader()
     const data = await deleteThread(id, path)
+    hideLoader()
     toast({ title: "Deleted Succesfully" })
     if (data?.success === true) {
     }
